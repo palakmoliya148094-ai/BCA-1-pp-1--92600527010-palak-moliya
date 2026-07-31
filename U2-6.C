164f28c69eb74d088f169f5aeb6e3d5e4fc5005a
palakmoliya148094-ai <palak.moliya148094@marwadiuniversity.ac.in>
2026-07-31 10:56:02 +0530

    Add files via upload

---
+++ U2-6.C
@@ -0,0 +1,19 @@
+//wap that find average
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z,avg,total;
+	clrscr();
+
+	printf("\n enter value of x,y,z");
+	scanf("%d%d%d", &x,&y,&z);
+
+	total=x+y+z;
+	avg=total/3;
+
+	printf("\n average is %d",avg);
+	getch();
+
+}
